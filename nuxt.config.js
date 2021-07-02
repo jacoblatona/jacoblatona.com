9a3ed511f45233980e259233d1385c8528f25d26
Jake <jacoblatona@Jacobs-MBP.home>
2021-07-02 15:52:19 -0500

    fix meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "mysite",
+    title: "Jacob Latona - Software Developer",
     htmlAttrs: {
       lang: "en"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Full Stack Web and App developer with 8+ years experience specializing in the esports and gaming industries."
+      },
       { name: "format-detection", content: "telephone=no" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
